12186f603b6be28e6771937ed131abf18a438e84
AndrzejRajczyk <rajczyk.andrzej@gmail.com>
2023-03-05 16:25:49 +0100

    First Commit

---
+++ Psssss/Psst.py
@@ -0,0 +1,42 @@
+
+
+print("Psst...")
+print(True > False)
+print(True < False)
+print('"Ucze sie"\n""jezyka"" \n"""Python"""')
+##########################
+kilometry = 12.25
+mile = 7.38
+
+mile_na_kilometry =  mile*1.61
+kilometry_na_mile =  kilometry/1.61
+
+print(mile, "mil to", round(mile_na_kilometry, 2), "km")
+print(kilometry, "km to", round(kilometry_na_mile, 2), "mil")
+##########################################################
+
+x =  0
+x = float(x)
+y=3*(x**3)-2*(x**2)+3*x-1
+print("y =", y)
+x =  1
+x = float(x)
+y=3*(x**3)-2*(x**2)+3*x-1
+print("y =", y)
+x =  -1
+x = float(x)
+y=3*(x**3)-2*(x**2)+3*x-1
+print("y =", y)
+
+
+
+#################
+print("Powiedz mi cokolwiek...")
+cokolwiek = input()
+print("Hmm...", cokolwiek, "... Serio?")
+################
+imie = input("Prosze, podaj mi swoje imie: ")
+nazwisko = input("Podaj mi rowniez swoje nazwisko:  ")
+print("Dziekuje.")
+print("\nNazywasz sie " + imie + " " + nazwisko + ".")
+print ("koniec programu")
